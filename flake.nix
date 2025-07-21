a50b3203efd9fd37d9bdb8b4b96c07d314eeff38
charlie <charlie@charlie.com>
2025-07-21 12:47:07 -0300

    chore: fix flake install

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Simple music search and download tool using deemix.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
